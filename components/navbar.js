da54181d1c79eba8abb38adfb19377ac86391ccb
bonevibes <87565205+bonevibes@users.noreply.github.com>
2022-04-18 13:51:04 +0530

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -112,7 +112,7 @@ const Navbar = props => {
                 </NextLink>
                 <MenuItem
                   as={Link}
-                  href="https://github.com/craftzdog/craftzdog-homepage"
+                  href="https://github.com/bonevibes/portfolio"
                 >
                   View Source
                 </MenuItem>
